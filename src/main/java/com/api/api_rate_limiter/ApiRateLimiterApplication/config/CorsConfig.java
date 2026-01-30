eab3e6d0278ce679a3d36425a64c343f33ac4149
Chinmayananda Tripathy <149191975+chinmayTripathy15@users.noreply.github.com>
2026-01-30 19:02:54 +0530

    Update CorsConfig.java

--- src/main/java/com/api/api_rate_limiter/ApiRateLimiterApplication/config/CorsConfig.java
+++ src/main/java/com/api/api_rate_limiter/ApiRateLimiterApplication/config/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
 
@@ -16,11 +17,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")   // ✅ allow all (frontend + render)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
     }
-
 }
